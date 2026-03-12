68512aaea5e2eac15e3ea818adebbd0331f1c0bd
Ivan Leshchenko <leshchenkoivan@gmail.com>
2026-03-12 17:38:51 +0200

    feat: favicon and title changed

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-	base: '/fop-calc/',
+export default defineConfig(({ command }) => ({
+	base: command === 'build' ? '/fop-calc/' : '/',
 	plugins: [react()],
 	resolve: {
 		alias: {
 			'@src': path.resolve(__dirname, './src'),
 		},
 	},
-})
+}))
